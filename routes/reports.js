07f75a2b9816437c258b8da6b0eaeab37fe7745f
Syraff <72563332+Syraff@users.noreply.github.com>
2023-09-22 21:58:29 +0800

    edit: route

--- routes/reports.js
+++ routes/reports.js
@@ -2,9 +2,8 @@ const express = require("express");
 const ReportController = require("../controllers/reportController");
 const report = express.Router();
 
-report.get("/comments", ReportController.getReport);
-report.post("/comments", ReportController.postReport);
-report.get("/comments/:id", ReportController.getReportById);
-
+report.get("/", ReportController.getReport);
+report.post("/", ReportController.postReport);
+report.get("/:id", ReportController.getReportById);
 
 module.exports = report;

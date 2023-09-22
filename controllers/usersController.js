e49a12f0f432b51dbc9ee3c79782227bf14b7404
Syraff <72563332+Syraff@users.noreply.github.com>
2023-09-22 19:43:16 +0800

    feat: CRD Investor

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,55 @@
-class UserController {}
+const { compare } = require("../helpers/bcrypt");
+const { token } = require("../helpers/jwt");
+const { Investor, Farmer } = require("../models");
+class UserController {
+  static async findInvestor(req, res, next) {
+    try {
+      const data = await Investor.findAll();
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
+    }
+  }
+  static async findOneInvestor(req, res, next) {
+    try {
+      const data = await Investor.findByPk(req.params.id);
+      if (!data) throw { name: "investor_not_found", id: req.params.id };
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
+    }
+  }
+  static async createInvestor(req, res, next) {
+    try {
+      const { username, email, password, phoneNumber } = req.body;
+      const data = await Investor.create({
+        username,
+        email,
+        password,
+        phoneNumber,
+      });
+      const access_token = token({ id: data.id });
+      res.status(201).json({ access_token });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async loginInvestor(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) throw { name: "email/password_required" };
+      if (!password) throw { name: "email/password_required" };
+      const user = await Investor.findOne({ where: { email } });
+      if (!user) throw { name: "invalid_email/password" };
+      const valid = compare(password, user.password);
+      if (!valid) throw { name: "invalid_email/password" };
+      const access_token = token({ id: user.id });
+      res.status(200).json({ access_token });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
 
 module.exports = UserController;
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,14 +2,31 @@ const errorHandler = (err, req, res, next) => {
   let status = 500;
   let message = "internal server error";
 
-  if (err.name === "InvalidFarmId") {
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    status = 400;
+    message = err.errors[0].message;
+  } else if (err.name === "investor_not_found") {
+    status = 404;
+    message = `investor with id ${err.id} not found`;
+  } else if (err.name === "email/password_required") {
+    status = 400;
+    message = "Email/Password required";
+  } else if (err.name === "invalid_email/password") {
+    status = 400;
+    message = "Email/Password invalid";
+  } else if (err.name === "not_found") {
+    status = 404;
+    message = "not found";
+  } else if (err.name === "InvalidFarmId") {
     status = 404;
     message = "Farm not found";
   } else if (err.name === "not_found") {
     status = 404;
     message = "not found";
   }
-
   res.status(status).json({ message: message });
 };
 

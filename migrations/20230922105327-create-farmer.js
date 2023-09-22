641f84a85894c676f6d84494dc248256052fb6d8
Syraff <72563332+Syraff@users.noreply.github.com>
2023-09-22 21:52:08 +0800

    del: addres from migration

--- migrations/20230922105327-create-farmer.js
+++ migrations/20230922105327-create-farmer.js
@@ -19,9 +19,6 @@ module.exports = {
       password: {
         type: Sequelize.STRING,
       },
-      address: {
-        type: Sequelize.STRING,
-      },
       phoneNumber: {
         type: Sequelize.STRING,
       },

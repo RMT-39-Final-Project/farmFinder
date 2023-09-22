7ed137e2a620226d42bba5173488e914dde20005
gavrilariendra <gavrilariendra00@gmail.com>
2023-09-22 20:48:59 +0700

    feat: CR for report

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,13 @@
-const errorHandler = (req, res, next) => {
+const errorHandler = (req, res, error, next) => {
   let status = 500;
   let message = "internal server error";
+
+  console.log(error);
+
+  if (error.name === "Bad request") {
+    status = 400;
+    message = "Description cannot be empty!";
+  }
 
   res.status(status).json({ message: message });
 };

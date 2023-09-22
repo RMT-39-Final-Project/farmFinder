0fb8354f4821ca54065dcba69402d32ab88bf233
Juan Kevin Tri Harto <juankevintriharto@gmail.com>
2023-09-23 07:48:36 +1200

    added model and seeding

--- routes/farm.js
+++ routes/farm.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const FarmController = require("../controllers/farmsController");
 const farm = express.Router();
+
+farm.get('/',(req, res) => {
+  res.send('Hello World!')
+})
+farm.get('/:id',(req, res) => {
+  res.send('Hello World!')
+})
+farm.post('/',(req, res) => {
+  res.send('Hello World!')
+})
+farm.get('/my-farms',(req, res) => {
+  res.send('Hello World!')
+})
+farm.get('/my-farms/:id',(req, res) => {
+  res.send('Hello World!')
+})
+farm.delete('/my-farms/:id',(req, res) => {
+  res.send('Hello World!')
+})
+
 
 module.exports = farm;

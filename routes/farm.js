856cb43c837a787faa29ec0ee69bf0fd027b5579
Juan Kevin Tri Harto <juankevintriharto@gmail.com>
2023-09-23 20:21:20 +1200

    farmFeatures

--- routes/farm.js
+++ routes/farm.js
@@ -4,13 +4,18 @@ const authorizeDeleteFarm = require("../middlewares/authorization");
 const farm = express.Router();
 
 
-farm.get('/', FarmController.getAllFarms)
-farm.get('/my-farms', FarmController.getAllUserFarms);
-farm.post('/',FarmController.addFarm)
-farm.get('/:farmId',FarmController.getFarmById)
-farm.get('/my-farms/:farmId',FarmController.getUserFarmById) // ini kayaknya g butuh si bisa pake /farms/:id aja tp just in case
-farm.delete('/my-farms/:farmId',authorizeDeleteFarm,FarmController.deleteByFarmId)
-farm.patch('/farms/:id',FarmController.updateFarmStatus)
+farm.get('/', FarmController.getAllFarms);
 
+farm.get('/my-farms/:farmId', FarmController.getUserFarmById);
+farm.get('/:farmId', FarmController.getFarmById);
+
+farm.get('/my-farms', FarmController.getAllUserFarms); //route in kehalang sama '/' 
+farm.post('/my-farms', FarmController.addFarm);
+farm.delete('/my-farms/:farmId', authorizeDeleteFarm, FarmController.deleteByFarmId);
+
+
+farm.patch('/:farmId', FarmController.updateFarmStatus);
 
 module.exports = farm;
+
+

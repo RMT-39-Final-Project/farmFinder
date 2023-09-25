d757e5354548ff26159692ada7d2f614817a019c
afuadlabib <afuad.lab@gmail.com>
2023-09-25 23:17:06 +0700

    test: report done

--- __test__/report.test.js
+++ __test__/report.test.js
@@ -36,8 +36,9 @@ beforeAll(async () => {
       balance: 1000000,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },
+    }
   ]);
+
   await sequelize.queryInterface.bulkInsert("Investors", [
     {
       username: "John Doe",
@@ -47,9 +48,19 @@ beforeAll(async () => {
       balance: 100000,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },
+    }
   ]);
+
+  farm = farm.map((e)=>{
+    e.createdAt = e.updatedAt = new Date()
+    return e
+  })
+
   await sequelize.queryInterface.bulkInsert("Farms", farm);
+  report = report.map((e)=>{
+    e.createdAt = e.updatedAt = new Date()
+    return e
+  })
   await sequelize.queryInterface.bulkInsert("Reports", report);
 });
 
@@ -75,16 +86,16 @@ afterAll(async () => {
     restartIdentity: true,
   });
 });
+describe("Report Test", ()=>{
 
 describe("GET /reports", () => {
-  it("responds with status 200 when success get reports", async () => {
-    const response = await request(app).get("/reports");
-    expect(response.status).toBe(200);
-    expect(response.body).toBeInstanceOf(Object);
-    expect(response.body).toHaveProperty("reports");
-    expect(response.body).toHaveProperty("description");
-  });
+  it("should response with status 200 when Success", async () => {
+    const response = await request(app).get("/reports")
+    expect(response.status).toBe(200)
+  })
 });
+})
+
 
 describe("POST /reports", () => {
   it("responds with status 201 when success post reports", async () => {
@@ -93,20 +104,22 @@ describe("POST /reports", () => {
       farmId: 1,
       description: "Very good service, good land to invest here or build a business",
     });
+
     expect(response.status).toBe(201);
     expect(response.body).toBeInstanceOf(Object);
-    expect(response.body).toHaveProperty("investorId");
-    expect(response.body).toHaveProperty("farmId");
-    expect(response.body).toHaveProperty("description");
+    expect(response.body).toHaveProperty('message', 'New description added');
+
   });
 });
 
 describe("GET /reports/:id", () => {
   it("responds with status 200 when success get reports by id", async () => {
-    const response = await request(app).get("/reports/:id");
+    const response = await request(app).get("/reports/1");
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Object);
-    expect(response.body).toHaveProperty("reports");
+
+    expect(response.body).toHaveProperty("investorId");
     expect(response.body).toHaveProperty("description");
+    expect(response.body).toHaveProperty("farmId");
   });
 });

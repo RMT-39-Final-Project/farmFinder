075bb41d7941de5771751e54893ac7762b9ac0f3
Syraff <72563332+Syraff@users.noreply.github.com>
2023-09-28 13:40:03 +0800

    fix auth getallmyfamr

--- routes/farm.js
+++ routes/farm.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const FarmController = require('../controllers/farmsController');
-const authorizeDeleteFarm = require('../middlewares/authorization');
-const { authFarmer } = require('../middlewares/auth');
+const express = require("express");
+const FarmController = require("../controllers/farmsController");
+const authorizeDeleteFarm = require("../middlewares/authorization");
+const { authFarmer } = require("../middlewares/auth");
 const farm = express.Router();
 
 farm
 
-  .get('/', FarmController.getAllFarms)
-  .get('/:farmId', FarmController.getFarmById)
-  .get('/my-farms/farms', FarmController.getAllMyFarm)
+  .get("/", FarmController.getAllFarms)
+  .get("/:farmId", FarmController.getFarmById)
   .use(authFarmer)
+  .get("/my-farms/farms", FarmController.getAllMyFarm)
   .delete(
-    '/my-farms/:farmId',
+    "/my-farms/:farmId",
     authorizeDeleteFarm,
     FarmController.deleteByFarmId
-    )
-    .get('/my-farms/farm', FarmController.getAllUserFarms)
-    .post('/my-farms/farm', FarmController.addFarm)
-    .get('/my-farms/:farmId', FarmController.getUserFarmById)
-  .patch('/:farmId', FarmController.updateFarmStatus);
+  )
+  .get("/my-farms/farm", FarmController.getAllUserFarms)
+  .post("/my-farms/farm", FarmController.addFarm)
+  .get("/my-farms/:farmId", FarmController.getUserFarmById)
+  .patch("/:farmId", FarmController.updateFarmStatus);
 
 module.exports = farm;
